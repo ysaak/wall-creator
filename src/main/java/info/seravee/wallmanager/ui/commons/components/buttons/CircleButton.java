cc95502369627fd583beca23b26047497946c77e
Damien ROTH <damien.roth@gmail.com>
2016-05-09 13:55:11 +0200

    Add basics of MD buttons

--- src/main/java/info/seravee/wallmanager/ui/commons/components/buttons/CircleButton.java
+++ src/main/java/info/seravee/wallmanager/ui/commons/components/buttons/CircleButton.java
@@ -6,24 +6,16 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Insets;
-import java.awt.Point;
 import java.awt.Shape;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.geom.Area;
 import java.awt.geom.Ellipse2D;
 
 import javax.swing.Action;
 import javax.swing.BorderFactory;
 import javax.swing.Icon;
-import javax.swing.JButton;
-import javax.swing.Timer;
 
 import info.seravee.wallmanager.ui.commons.laf.LafUtils;
 
-public class CircleButton extends JButton {
+public class CircleButton extends RippleButton {
 	private static final long serialVersionUID = 7240370990302640076L;
 
 	public CircleButton(Action action) {
@@ -38,18 +30,11 @@ public class CircleButton extends JButton {
 		setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
 
 		initShape();
-		
-		addMouseListener(new MouseAdapter() {
-			
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				startRippleEffect(e.getPoint());
-			}
-		});
 	}
+	
+	protected Shape base;
 
-	protected Shape shape, base;
-
+	@Override
 	protected void initShape() {
 		if (!getBounds().equals(base)) {
 			Dimension s = getPreferredSize();
@@ -64,75 +49,6 @@ public class CircleButton extends JButton {
 		Insets i = getInsets();
 		int iw = Math.max(icon.getIconWidth(), icon.getIconHeight());
 		return new Dimension(iw + i.right + i.left, iw + i.top + i.bottom);
-	}
-
-	private static final int RIPPLE_DELAY = 35;
-	private static final int RIPPLE_DIAMETER_INC_VALUE = 5; 
-	private static final Color RIPPLE_COLOR = new Color(1, 1, 1, 0.2f);
-	
-	private Area rippleArea;
-	private Point rippleStartPoint = null;
-	private int rippleDiameter = 0;
-	private Timer rippleTimer = null;
-	
-	
-	protected void startRippleEffect(Point mousePosition) {
-		if (rippleTimer != null) {
-			stopRippleEffect();
-		}
-		
-		rippleStartPoint = new Point(mousePosition);
-		rippleDiameter = 0;
-		
-		rippleTimer = new Timer(RIPPLE_DELAY, new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				rippleEffect();
-			}
-		});
-		rippleTimer.setInitialDelay(0);
-		rippleTimer.start();
-	}
-
-	protected void rippleEffect() {
-		// https://github.com/traex/RippleEffect/blob/master/library/src/main/java/com/andexert/library/RippleView.java
-		// https://github.com/balysv/material-ripple/blob/master/library/src/main/java/com/balysv/materialripple/MaterialRippleLayout.java
-		
-		initShape();
-		
-    	// Increase diameter
-		rippleDiameter += RIPPLE_DIAMETER_INC_VALUE;
-		
-		// Adjust value for X/Y coordinate
-		int coordAdjust = rippleDiameter / 2;
-		
-		final Shape rippleShape = new Ellipse2D.Float(rippleStartPoint.x - coordAdjust, rippleStartPoint.y - coordAdjust, rippleDiameter, rippleDiameter);
-		final Area shapeArea = new Area(shape);
-		
-		rippleArea = new Area(rippleShape);
-		rippleArea.intersect(shapeArea);
-		
-		
-		repaint();
-		
-		shapeArea.subtract(rippleArea);
-		
-		if (shapeArea.isEmpty()) {
-			stopRippleEffect();
-		}
-	}
-	
-	protected void stopRippleEffect() {
-		// Stop timer
-		if (rippleTimer != null) {
-			rippleTimer.stop();
-			rippleTimer = null;
-		}
-		
-		// Reset vars
-		rippleStartPoint = null;
-		rippleDiameter = 0;
 	}
 
 	@Override
@@ -160,19 +76,7 @@ public class CircleButton extends JButton {
 	    	g2.fill(shape);
 	    }
 	    
-	    if (rippleStartPoint != null) {
-	    	
-	    	g2.setColor(RIPPLE_COLOR);
-	    	
-	    	int coordAdjust = rippleDiameter / 2;
-	    	
-	    	Shape rippleShape = new Ellipse2D.Float(rippleStartPoint.x - coordAdjust, rippleStartPoint.y - coordAdjust, rippleDiameter, rippleDiameter);
-	    	
-	    	Area rippleArea = new Area(rippleShape);
-	    	rippleArea.intersect(new Area(shape));
-	    	
-	    	g2.fill(rippleArea);
-	    }
+	    
 	    
 		super.paintComponent(g);
 	}
